65e74fce59607917b1db5475d0d0cb394ea4555a
Jung <tht1234551@gmail.com>
2022-12-14 21:20:15 +0900

    #9 deploy test

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const port = 80;
+const port = parseInt(process.env.PORT) || 80;
+console.log(port)
 
 const router = require('./routes/routes');
 app.use('/', router);
+
 
 app.listen(port, () => {
     console.log(`app is running on PORT ${port}`)
@@ -12,3 +14,4 @@ app.listen(port, () => {
 // const db = require('./db_connection')
 
 module.exports = app;
+
